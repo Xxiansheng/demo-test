8911cd175fdd45597f1aecf9f1255df883b4fd9d
xxj <541242198@qq.com>
2018-04-13 17:33:26 +0800

    add SaladActivity

---
+++ app/src/main/java/com/example/inject/bean/SaladActivity.java
@@ -0,0 +1,18 @@
+package com.example.inject.bean;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xuxinji
+ * 2018/4/13
+ */
+
+public class SaladActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
